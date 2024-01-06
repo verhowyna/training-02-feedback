a317393e87342bee5e8aed39590c2ffdd2db0d97
verhowyna <verhowyna@ukr.net>
2024-01-06 21:55:38 +0200

    commit-5

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,21 @@
-export const Statistics = () => {
-    return (
-        <div></div>
-    )
-}
+import { Notification } from 'components/Notification/Notification';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return total ? (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>PositivePercentage: {positivePercentage}%</p>
+    </div>
+  ) : (
+    <Notification message="There is no feedback" />
+  );
+};
